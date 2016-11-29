58b99e0cc506fb9fc35a166c3ce036ae2624b87f
Jack Dickman <jackdman7@yahoo.com>
2016-11-29 15:21:37 -0500

    Update Display.java

--- Display.java
+++ Display.java
@@ -17,6 +17,25 @@ public class Display extends JPanel {
 	private Board board;
 	private Scanner in;
 	private int best;
+	
+	//---------------------------------------------------------------------
+	
+	private Color background = new Color(249, 249, 242);
+	private Color outline = new Color(170, 160, 155);
+	private Color emptysquare = new Color(196, 187, 178);
+	private Color two = new Color(239, 239, 232);
+	private Color four = new Color(236, 236, 210);
+	private Color eight = new Color(255, 178, 102);
+	private Color sixteen = new Color(255, 144, 110);
+	private Color thirtytwo = new Color(255, 111, 67);
+	private Color sixtyfour = new Color(255, 87, 36);
+	private Color onetwentyeight = new Color(255, 230, 133);
+	private Color twofiftysix = new Color(255, 224, 94);
+	private Color fivetwelve = new Color(255, 217, 61);
+	private Color tentwentyfour = new Color(255, 207, 13);
+	private Color twentyfortyeight = new Color(245, 196, 0);
+	private Color fortyninetysix = new Color(255, 119, 119);
+	private Color eightyoneninetytwo = new Color(255, 71, 71);
 
 	//---------------------------------------------------------------------
 
@@ -71,19 +90,73 @@ public class Display extends JPanel {
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		//draw light background
-		g.setColor(new Color(249, 249, 242));
+		g.setColor(background);
 		g.fillRect(0, 0, 400, 645);
+
+		//draw dark outline part
+		g.setColor(outline);
+		g.fillRoundRect(20, 240, 360, 360, 10, 10);
+
+		//draw squares
+		for(int x = 28; x <= 292; x += 88)  {
+			for(int y = 248; y <= 512; y += 88) {
+				int z = board.get((y - 200) / 88, x / 88);
+				if(z == 0) {
+					g.setColor(emptysquare);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  2) {
+					g.setColor(two);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  4) {
+					g.setColor(four);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  8) {
+					g.setColor(eight);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  16) {
+					g.setColor(sixteen);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  32) {
+					g.setColor(thirtytwo);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  64) {
+					g.setColor(sixtyfour);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  128) {
+					g.setColor(onetwentyeight);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  256) {
+					g.setColor(twofiftysix);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  512) {
+					g.setColor(fivetwelve);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  1024) {
+					g.setColor(tentwentyfour);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  2048) {
+					g.setColor(twentyfortyeight);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  4096) {
+					g.setColor(fortyninetysix);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				} else if(z ==  8192) {
+					g.setColor(eightyoneninetytwo);
+					g.fillRoundRect(x, y, 80, 80, 7, 7);
+				}
+			}
+		}
 
 		//draw yellow box w/ 2048
 		g.setColor(new Color(245, 196, 0));
-		g.fillRect(20, 20, 140, 140);
+		g.fillRoundRect(20, 20, 140, 140, 10, 10);
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("Arial Black", Font.PLAIN, 45));
 		g.drawString("2048", 30, 106);
 
 		//draw score box and "SCORE"
-		g.setColor(new Color(170, 160, 155));
-		g.fillRect(180, 20, 90, 70);
+		g.setColor(outline);
+		g.fillRoundRect(180, 20, 90, 70, 10, 10);
 		g.setColor(new Color(230, 224, 220));
 		g.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		g.drawString("SCORE", 192, 46);
@@ -105,8 +178,8 @@ public class Display extends JPanel {
 		}
 
 		//draw best box and "BEST"
-		g.setColor(new Color(170, 160, 155));
-		g.fillRect(290, 20, 90, 70);
+		g.setColor(outline);
+		g.fillRoundRect(290, 20, 90, 70, 10, 10);
 		g.setColor(new Color(230, 224, 220));
 		g.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		g.drawString("BEST", 310, 46);
@@ -129,14 +202,14 @@ public class Display extends JPanel {
 
 		//draw menu box and "MENU"
 		g.setColor(new Color(255, 163, 101));
-		g.fillRect(180, 110, 90, 50);
+		g.fillRoundRect(180, 110, 90, 50, 10, 10);
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		g.drawString("MENU", 197, 141);
 
 		//draw leaderboard box and "LEADERBOARD"
 		g.setColor(new Color(255, 163, 101));
-		g.fillRect(290, 110, 90, 50);
+		g.fillRoundRect(290, 110, 90, 50, 10, 10);
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("Arial Black", Font.PLAIN, 10));
 		g.drawString("LEADERBOARD", 293, 139);
@@ -145,60 +218,6 @@ public class Display extends JPanel {
 		g.setColor(new Color(111, 102, 93));
 		g.setFont(new Font("Arial Black", Font.PLAIN, 15));
 		g.drawString("Join the numbers and get to the 2048 tile!", 28, 207);
-
-		//draw dark outline part
-		g.setColor(new Color(170, 160, 155));
-		g.fillRect(20, 240, 360, 360);
-
-		//draw squares
-		for(int x = 28; x <= 292; x += 88)  {
-			for(int y = 248; y <= 512; y += 88) {
-				int z = board.get((y - 200) / 88, x / 88);
-				if(z == 0) {
-					g.setColor(new Color(196, 187, 178));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  2) {
-					g.setColor(new Color(239, 239, 232));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  4) {
-					g.setColor(new Color(236, 236, 210));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  8) {
-					g.setColor(new Color(255, 178, 102));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  16) {
-					g.setColor(new Color(255, 144, 110));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  32) {
-					g.setColor(new Color(255, 111, 67));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  64) {
-					g.setColor(new Color(255, 87, 36));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  128) {
-					g.setColor(new Color(255, 230, 133));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  256) {
-					g.setColor(new Color(255, 224, 94));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  512) {
-					g.setColor(new Color(255, 217, 61));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  1024) {
-					g.setColor(new Color(246, 197, 0));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  2048) {
-					g.setColor(new Color(245, 196, 0));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  4096) {
-					g.setColor(new Color(255, 119, 119));
-					g.fillRect(x, y, 80, 80);
-				} else if(z ==  8192) {
-					g.setColor(new Color(255, 71, 71));
-					g.fillRect(x, y, 80, 80);
-				}
-			}
-		}
 
 		//draw numbers
 		for(int r = 0; r < 4; r++) {
